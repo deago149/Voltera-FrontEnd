79ef355a60fea29cceac461f858adfcc48f78548
deago149 <36834728+deago149@users.noreply.github.com>
2025-07-18 10:53:23 +1000

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-   output: 'export',
+  output: 'export',
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3000',
+    // Add other env variables here if needed
+  },
 };
 
 export default nextConfig;
