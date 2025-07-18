0f8cf0b8d637b47340f8bca8bc4615e8322ed063
Eli Tamakloe <ternimus@Elis-MacBook-Pro.local>
2025-07-18 11:16:58 +1000

    updated build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   // Tell Next.js to use /src as the base directory
+    env: {
+    API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3000',
+    // Add other env variables here if needed
+  },
   experimental: {
     appDir: true,
   },
