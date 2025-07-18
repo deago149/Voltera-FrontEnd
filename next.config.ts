2b1ff1dfda924e92a09cf5b8e307bad79f74778f
Eli Tamakloe <ternimus@Elis-MacBook-Pro.local>
2025-07-18 11:23:15 +1000

    updated build

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,6 @@ const nextConfig: NextConfig = {
     API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3000',
     // Add other env variables here if needed
   },
-  experimental: {
-    appDir: true,
-  },
 };
 
 export default nextConfig;
